c9972b389183a12d263556b84df96388f415f3c3
WangJie <641156127@qq.com>
2017-05-26 14:45:29 +0800

    新增了Person类

---
+++ WeChat/WeChat/Person/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  WeChat
+//
+//  Created by 王杰 on 2017/5/26.
+//  Copyright © 2017年 杰王子. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ WeChat/WeChat/Person/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  WeChat
+//
+//  Created by 王杰 on 2017/5/26.
+//  Copyright © 2017年 杰王子. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
